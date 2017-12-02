6b0470522642ca63ab83209cbf3df43fe12c7a92
Kristen Warnquist <kristen.warnquist@gmail.com>
2017-12-02 19:26:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,8 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.uppercase
-    "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   elsif phrase == phrase.uppercase && phrase == "I LOVE YOU GRANDMA"
-    "I LOVE YOU TOO PUMPKIN!"
-  else "HUH?! SPEAK UP, SONNY!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else puts "HUH?! SPEAK UP, SONNY!"
 end
